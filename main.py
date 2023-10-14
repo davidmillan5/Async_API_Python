6b971d8ef7c7f7a54a1945a3b530e96f926f4b62
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-10-13 20:44:20 -0500

    Code completed

--- main.py
+++ main.py
@@ -1,37 +1,81 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from crud import CRUD
+from db import engine
+from schemas import NoteModel, NoteCreateModel
+from http import HTTPStatus
+from typing import List
+from models import Note
+from fastapi.responses import JSONResponse
+import uuid
 
 app = FastAPI(
-    title='Noted API',
-    description='This a simple note taking service',
-    docs_url='/'
+    title="Noted API", description="This is a simple note taking service", docs_url="/"
 )
 
+#create an async session object for CRUD
+session = async_sessionmaker(bind=engine, expire_on_commit=False)
 
-@app.get('/')
-async def hello():
-    return {'message': 'Hello World'}
+db = CRUD()
 
 
-@app.get('/notes')
+@app.get("/notes", response_model=List[NoteModel])
 async def get_all_notes():
-    pass
+    """API endpoint for listing all note resources
+    """
+    notes = await db.get_all(session)
+
+    return notes
 
 
-@app.post('/notes')
-async def create_note():
-    pass
+@app.post("/notes", status_code=HTTPStatus.CREATED, response_model=NoteModel)
+async def create_note(note_data: NoteCreateModel) -> NoteModel:
+    """API endpoint for creating a note resource"""
+    new_note = Note(
+        id=str(uuid.uuid4()), title=note_data.title, content=note_data.content
+    )
+
+    note = await db.add(session, new_note)
+    return note
 
 
-@app.get('/notes/{note_id}')
-async def get_note_by_id(note_id):
-    pass
+@app.get("/note/{note_id}", response_model=NoteModel)
+async def get_note_by_id(note_id: str):
+    """API endpoint for retrieving a note by its ID"""
+    note = await db.get_by_id(session, note_id)
+    if not note:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Note not found")
+    return note
 
 
-@app.patch('/notes/{note_id}')
-async def update_note(note_id):
-    pass
+@app.patch("/note/{note_id}")
+async def update_note(note_id: str, data: NoteCreateModel):
+    """Update by ID
+
+    Args:
+        note_id (str): ID of note to update
+        data (NoteCreateModel): data to update note
+
+    Returns:
+        dict: the updated note
+    """
+    note = await db.update(
+        session, note_id, data={"title": data.title, "content": data.content}
+    )
+
+    return note
 
 
-@app.delete('/notes/{note_id}')
-async def delete_note(note_id):
-    pass
+@app.delete("/note/{note_id}", status_code=HTTPStatus.NO_CONTENT)
+async def delete_note(note_id) -> None:
+    """Delete note by id
+
+    Args:
+        note_id (str): ID of note to delete
+
+    """
+    note = await db.get_by_id(session, note_id)
+
+    result = await db.delete(session, note)
+
+    return result
--- models.py
+++ models.py
@@ -9,16 +9,16 @@ class Note:
     title str
     content str
     date_created datetime
+
 """
 
-
+#the database model for notes
 class Note(Base):
-    __tablename__ = 'notes'
-
+    __tablename__ = "notes"
     id: Mapped[str] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(nullable=False)
     content: Mapped[str] = mapped_column(Text, nullable=False)
-    date_created: Mapped[datetime] = mapped_column(default=datetime.utcnow())
+    date_created: Mapped[datetime] = mapped_column(default=datetime.utcnow)
 
     def __repr__(self) -> str:
         return f"<Note {self.title} at {self.date_created}>"
